7f6ee2a1bdb63ffb35990605ae0791a20c2f1e63
MAHDI BELABBAS <mahdibelabbas@gmail.com>
2024-10-29 15:06:36 -0400

    UserService class added

---
+++ src/main/java/com/example/e_management_store_server/service/UserService.java
@@ -0,0 +1,28 @@
+package com.example.e_management_store_server.service;
+
+import com.example.e_management_store_server.model.User;
+import com.example.e_management_store_server.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    public User postUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public void cleanUpUserTable() {
+        jdbcTemplate.execute("SET SQL_SAFE_UPDATES = 0;");
+        jdbcTemplate.execute("DELETE FROM users");
+        jdbcTemplate.execute("ALTER TABLE users AUTO_INCREMENT = 1;");
+        jdbcTemplate.execute("SET SQL_SAFE_UPDATES = 1;");
+    }
+
+
+}
