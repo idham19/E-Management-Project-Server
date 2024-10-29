e1a4c6e06dfb295e2d8129da3309d5fcfdd84f9f
MAHDI BELABBAS <mahdibelabbas@gmail.com>
2024-10-29 12:13:04 -0400

    initial commit

---
+++ src/main/java/com/example/e_management_store_server/EManagementStoreServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.e_management_store_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EManagementStoreServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EManagementStoreServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/e_management_store_server/EManagementStoreServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.e_management_store_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EManagementStoreServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
