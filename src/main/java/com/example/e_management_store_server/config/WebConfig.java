8bbbef12ceeb9d04bbaee4b67fce6571dda04c9c
MAHDI BELABBAS <mahdibelabbas@gmail.com>
2024-11-01 15:56:27 -0400

    webConfig class added

---
+++ src/main/java/com/example/e_management_store_server/config/WebConfig.java
@@ -0,0 +1,18 @@
+package com.example.e_management_store_server.config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // Allow all endpoints
+                .allowedOrigins("http://localhost:4200") // Allow requests from your Angular app
+                .allowedMethods("GET", "POST", "PUT", "DELETE") // Specify allowed methods
+                .allowedHeaders("*") // Allow all headers
+                .allowCredentials(true); // Allow credentials if needed
+    }
+}
